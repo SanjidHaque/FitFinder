ecc69cb466a408472d0ffb0769cbbb621f56a836
SanjidHaque <sanjidulhaque@gmail.com>
2018-11-24 22:59:01 +0600

    Candidate attachments can be uploaded now. File size is unlimited.

--- FitFinderBackEnd/FitFinderBackEnd/Controllers/FitFinderController.cs
+++ FitFinderBackEnd/FitFinderBackEnd/Controllers/FitFinderController.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
 using System.Data.Entity.Validation;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Web;
 using System.Web.Http;
 using System.Web.Http.Cors;
 using FitFinderBackEnd.Models;
@@ -40,6 +42,30 @@ namespace FitFinderBackEnd.Controllers
             return Ok();
         }
 
+        [HttpPost]
+        [Route("api/UploadCandidateAttachment")]
+        public IHttpActionResult UploadCandidateAttachment()
+        {     
+            List<string> filePaths = new List<string>();
+            var httpRequest = HttpContext.Current.Request;
+            var candidateId = httpRequest["Candidate Id"];  
+            for (int i = 0; i<  httpRequest.Files.Count; i++)
+            {
+                var postedFile = httpRequest.Files[i];
+                var fileName = new String(Path.GetFileNameWithoutExtension(postedFile.FileName).Take(10).ToArray()).Replace(" ", "-");
+                fileName = fileName + DateTime.Now.ToString("yymmssfff") + Path.GetExtension(postedFile.FileName);
+                var filePath = HttpContext.Current.Server.MapPath("~/Content/Candidate Attachments/" + fileName);
+                postedFile.SaveAs(filePath);
+                filePaths.Add(filePath);         
+            }
+           List<CandidateAttachment> candidateAttachment =  _context.CandidateAttachments.Where(p => p.CandidateId == candidateId).ToList();
+            for (int j = 0; j < candidateAttachment.Count; j++)
+            {
+                candidateAttachment[j].FilePath = filePaths[j];
+                _context.SaveChanges();
+            }
+            return Ok();
+        }
 
         [HttpGet]
         [Route("api/Candidate")]
