86690883ac34cc3c5a33c7f4a1af59131d6080b9
SanjidHaque <sanjidulhaque@gmail.com>
2019-02-25 19:39:58 +0600

    Email confirmation only works in firefox edge. It is not working in chrome.

--- FitFinderBackEnd/FitFinderBackEnd/App_Start/IdentityConfig.cs
+++ FitFinderBackEnd/FitFinderBackEnd/App_Start/IdentityConfig.cs
@@ -42,7 +42,7 @@ namespace FitFinderBackEnd
             {
                 manager.UserTokenProvider = new DataProtectorTokenProvider<ApplicationUser>(dataProtectionProvider.Create("ASP.NET Identity"))
                 {
-                    TokenLifespan = TimeSpan.FromHours(3)
+                    TokenLifespan = TimeSpan.FromSeconds(3)
                 };
             }
             return manager;
